d0570a55c056d5a6a43148e837dc4e2a38946389
Mushtaq Ali <mushtaque@edx.org>
2017-02-02 20:52:22 +0500

    After rebase

--- cms/static/js/views/move_xblock_list.js
+++ cms/static/js/views/move_xblock_list.js
@@ -119,12 +119,9 @@ function($, Backbone, _, gettext, HtmlUtils, StringUtils, XBlockUtils, MoveXBloc
             this.render();
         },
 
-<<<<<<< HEAD
         /**
          * Set parent and child XBlock categories.
          */
-=======
->>>>>>> 075ea9b... refactoring tests
         setDisplayedXBlocksCategories: function() {
             this.parentInfo.category = XBlockUtils.getXBlockType(
                 this.parentInfo.parent.get('category'),
